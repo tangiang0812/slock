38ad91f49eb07798d52064a81270e8544f5d307f
Gnaig <tangiang0812@gmail.com>
2022-02-19 00:09:54 +0700

    gnaig's first commit

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 /* user and group to drop privileges to */
 static const char *user  = "gnaig";
-static const char *group = "nogroup";
+static const char *group = "gnaig";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#292d3e",     /* after initialization */
+	[INPUT] =  "#82aaff",   /* during input */
+	[FAILED] = "#ff5370",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
